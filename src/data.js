7490237be107f5212eca614e41e33219ded5ab01
Chandini Bonam <tejabonam8@gmail.com>
2025-12-30 17:05:47 +0530

    Second Commit

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ const commandss = [
   
           From the past five years, I've been dedicated to develop user-friendly and visually appealing web applications as a front-end developer. My primary focus has been on utilizing React.js to build interactive and dynamic user interfaces.</p>
           
-          <p>I have a strong understanding of HTML, CSS, and JavaScript, which are the building blocks of web development. With React.js, I've been able to create reusable components, manage state efficiently, and ensure high performance in web applications.</p>
+          <p> I have a strong understanding of HTML, CSS, and JavaScript, which are the building blocks of web development. With React.js, I've been able to create reusable components, manage state efficiently, and ensure high performance in web applications.</p>
           
           <p>Throughout my career, I've worked on various projects where I've collaborated closely with designers and backend developers to turn concepts into reality. I'm experienced in translating design mockups into responsive and perfect layouts, ensuring a seamless user experience across different devices and screen sizes.</p>
           
